0c477a1678e263d400734e5a10677be042787ccf
KENG <waengchaikkun@gmail.com>
2020-09-06 16:32:33 +0700

    Update mongodb.py

--- src/db/mongodb.py
+++ src/db/mongodb.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # client = MongoClient("mongodb://localhost:27017")  # host uri
-client = MongoClient("mongodb+srv://Durian:YFOOw7XRw0fpfec8@cluster0-zfjpn.gcp.mongodb.net/image_predition?retryWrites=true&w=majority")
+client = MongoClient("....for host uri....")
 db = client.image_predition  # Select the database
 image_details = db.imageData
 
